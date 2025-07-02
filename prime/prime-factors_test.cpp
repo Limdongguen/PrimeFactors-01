69fdd6504655ca643639c7ae87d24e4a4e728886
dongguen.lim <vs008@naver.com>
2025-07-02 10:08:46 +0900

    [feature] 45

--- prime/prime-factors_test.cpp
+++ prime/prime-factors_test.cpp
@@ -1,9 +1,14 @@
 #include "gmock/gmock.h"
 #include "prime-factors.cpp"
 
-
 TEST(PrimeFactors, Of1) {
 	PrimeFactors prime_factor;
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactors prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
